77745f39a1dac77adbde84a88c402928a0d54f79
pelmenos <vadimshvynda@gmail.com>
2023-10-24 22:31:33 +0700

    Первый уровень по ООП

---
+++ OOP/lvl1/task3.php
@@ -0,0 +1,78 @@
+<?php
+
+class Post
+{
+    protected $title;
+    protected $text;
+
+    public function __construct(string $title, string $text)
+    {
+        $this->title = $title;
+        $this->text = $text;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setTitle($title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    public function setText($text): void
+    {
+        $this->text = $text;
+    }
+}
+
+class Lesson extends Post
+{
+    private $homework;
+
+    public function __construct(string $title, string $text, string $homework)
+    {
+        parent::__construct($title, $text);
+        $this->homework = $homework;
+    }
+
+    public function getHomework(): string
+    {
+        return $this->homework;
+    }
+
+    public function setHomework(string $homework): void
+    {
+        $this->homework = $homework;
+    }
+}
+
+class PaidLesson extends Lesson
+{
+    private $price;
+
+    public function __construct(string $title, string $text, string $homework, float $price)
+    {
+        parent::__construct($title, $text, $homework);
+        $this->price = $price;
+    }
+
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+    public function setPrice($price): void
+    {
+        $this->price = $price;
+    }
+}
+
+$php_lesson = new PaidLesson('Урок о наследовании в PHP', 'Лол, кек, чебурек',
+                        'Ложитесь спать, утро вечера мудренее', 99.9);
+var_dump($php_lesson);
